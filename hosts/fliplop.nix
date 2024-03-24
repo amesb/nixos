55310ea1a2fd9052f73bbe777f7080ffa82bfff0
Bryan Ames <b130610@gmail.com>
2024-03-24 13:22:00 -0700

    disable kde again

--- hosts/fliplop.nix
+++ hosts/fliplop.nix
@@ -21,10 +21,6 @@
   # hostname
   networking.hostName = "fliplop"; # Define your hostname.
 
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
   programs.steam.enable = true;
   programs.gamemode = {
     enable = true;
