0d1645c323f500fc7de1337837cc9c8463c06648
Bryan Ames <b130610@gmail.com>
2024-03-23 23:49:05 -0700

    walking back the hdr stuff... too much for now, just wanna game in sdr

--- hosts/fliplop.nix
+++ hosts/fliplop.nix
@@ -35,14 +35,6 @@
 
   programs.steam.enable = true;
 
-  # enable plasma 6 to try hdr stuff
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # enable gamescope session also for hdr stuff
-  programs.steam.gamescopeSession.enable = true;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
